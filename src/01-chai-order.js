3e7493b205334e3263ad12af247eec86151de1b0
Prakash Jangid <prakashjangid7357@gmail.com>
2026-02-10 00:21:30 +0530

    Complete chai-order and rangoli-maker questions

--- src/01-chai-order.js
+++ src/01-chai-order.js
@@ -47,20 +47,57 @@
  */
 export function getChaiOrderLength(order) {
   // Your code here
+  if (typeof order !== "string") {
+    return -1;
+  }
+
+  if (order === "") {
+    return null;
+  }
+
+  return order.trim().length;
 }
 
 export function shoutChaiOrder(order) {
   // Your code here
+  if (typeof order !== "string" || order === "") {
+    return "";
+  }
+
+  return order.trim().toUpperCase();
 }
 
 export function whisperChaiOrder(order) {
   // Your code here
+
+  if (typeof order !== "string" || order === "") {
+    return "";
+  }
+
+  return order.trim().toLowerCase();
 }
 
 export function hasSpecialIngredient(order, ingredient) {
   // Your code here
+  if (typeof order !== "string" || typeof ingredient !== "string") {
+    return false;
+  }
+
+  if (order.toLowerCase().includes(ingredient.toLowerCase())) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 export function getFirstAndLastChar(order) {
   // Your code here
+  if (typeof order !== "string" || order.trim() === "") {
+    return null;
+  }
+
+  let first = order.trim().charAt(0);
+  let last = order.trim().at(-1);
+
+  return { first, last };
 }
